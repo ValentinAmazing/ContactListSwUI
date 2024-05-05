fb448a695e112c8b27a7b18d17c4c3f6142d978f
ValentinAmazing <podrezva@bk.ru>
2024-05-05 23:28:43 +0700

    Initial Commit

---
+++ ContactListSwUI/ContactListSwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwUIApp.swift
+//  ContactListSwUI
+//
+//  Created by Valentin on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
