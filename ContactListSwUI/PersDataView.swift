a0e546a0ed2d5a344fcd6f5582cf32071f36325b
ValentinAmazing <podrezva@bk.ru>
2024-05-06 06:45:16 +0700

    foregroundStyle for icin

--- ContactListSwUI/PersDataView.swift
+++ ContactListSwUI/PersDataView.swift
@@ -21,6 +21,7 @@ private struct PersData: View {
     var body: some View {
         HStack {
             Image(systemName: icon)
+                .foregroundStyle(.blue)
             Text(persData ?? "no data")
         }
         .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
